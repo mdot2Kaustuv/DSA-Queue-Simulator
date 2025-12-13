83b417573c67112923a43f3a84a2493ab1c783b6
codeKGB <kbustuv@gmail.com>
2025-12-13 12:30:43 +0545

    Implemented 2 different classes form managing lanes and roads

--- queues.py
+++ queues.py
@@ -1,15 +1,4 @@
 
-class Vehicle :
-    def __init__ (self,id,lane,road,time) :
-        self.id = id
-        self.lane = lane
-        self.road = road
-        self.time = time
-
-    def __str__ (self) :
-        return f'{self.id} {self.lane}{self.road} {self.time}'
-
-
 class queue :
     def __init__(self):
         self.queue = []
