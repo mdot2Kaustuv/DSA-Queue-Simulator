06bc5e8463e59a207aea4ddfcfcae6c8f75045f7
codeKGB <kbustuv@gmail.com>
2025-12-25 10:20:56 +0545

    Updated file format according to project standards

--- Vehicles.py
+++
@@ -1,9 +0,0 @@
-class Vehicle :
-    def __init__ (self,id,lane,road,time) :
-        self.id = id
-        self.lane = lane
-        self.road = road
-        self.time = time
-
-    def __str__(self) :
-        return f'{self.id} {self.lane}{self.road} {self.time}'
